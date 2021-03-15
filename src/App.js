ce5d66da2d0bcdbec0369ec68d07ab6e03c501d0
pruthvi10 <pruthvimohanlal10@gmail.com>
2021-03-15 01:10:58 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello React</h2>
-      <h6>My name is pruthvi!</h6>
+      {/* SOCIAL MEDIAS NAVBAR */}
+      <nav class="topnav bg-dark  ">
+        <div class="text-left">
+          <a href="/#" class="teal-text text-darken-0">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="/#" class="teal-text text-darken-0">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="/#" class="teal-text text-darken-0">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="/#" class="teal-text text-darken-0">
+            <i class="fas fa-envelope"></i>
+          </a>
+        </div>
+      </nav>
+      {/* THE ACTUAL NAVBAR */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <i className="fas fa-exclamation text-danger"></i> oopsDev
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-mdb-toggle="collapse"
+            data-mdb-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto pr-2">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">
+                  Pricing
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="wip">
+      <marquee
+        behavior="scroll"
+        direction="right"
+        className="deep-orange darken-1 p-1 text-black"
+      >
+        Website is under construnction... Visit me later ♥
+      </marquee>
+    </div>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
